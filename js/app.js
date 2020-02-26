f6c4009b4023d473cf4ac8db0530fbee33462399
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2020-02-26 13:02:00 +0000

    added async and state

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 console.log('Danesh');
-document.addEventListener('')
-const youtubeApiKey = ;
+const youtubeApiKey = 'AIzaSyDkPTSj7RXHi1kSBemfQYm9TNu8CIweTUg';
 const apiURL = 'https://www.googleapis.com/youtube/v3';
 const makanMustyId = 'UC167j-qCguSgHu20raA211w';
+const state = {};
+async function getYoutubeData() {
+  const response = await fetch(`${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`);
+  const data = await response.json();
+  console.log(data);
+  state.regionCode = data.regionCode;
+  return data.regionCode;
 
-fetch( `${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`)
-  .then(response => {
-    return response.json();
-  })
-  .then(responseJSON => {
-    console.log(responseJSON);
-  });
-  
+  // return Promise.resolve(fetch(`${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`)
+  // .then(response => response.json())
+  // .then(responseJSON => state.pageInfo = responseJSON.pageInfo));
+}
+
+const response = getYoutubeData();
+console.log(response, state);
