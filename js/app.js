9a4c939d2ec83ecab83bc5ccd86c49404db415bf
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2020-02-27 10:56:18 +0000

    added async await

--- js/app.js
+++ js/app.js
@@ -3,17 +3,21 @@ const youtubeApiKey = 'AIzaSyDkPTSj7RXHi1kSBemfQYm9TNu8CIweTUg';
 const apiURL = 'https://www.googleapis.com/youtube/v3';
 const makanMustyId = 'UC167j-qCguSgHu20raA211w';
 const state = {};
-async function getYoutubeData() {
-  const response = await fetch(`${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`);
-  const data = await response.json();
-  console.log(data);
-  state.regionCode = data.regionCode;
-  return data.regionCode;
 
-  // return Promise.resolve(fetch(`${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`)
-  // .then(response => response.json())
-  // .then(responseJSON => state.pageInfo = responseJSON.pageInfo));
-}
-
-const response = getYoutubeData();
-console.log(response, state);
+document.addEventListener('DOMContentLoaded', async () => {
+  async function getYoutubeData() {
+    const response = await fetch(`${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`);
+    const data = await response.json();
+    console.log(data);
+    state.regionCode = data.regionCode;
+    return data.regionCode;
+  
+    // return Promise.resolve(fetch(`${apiURL}/search?part=snippet&channelId=${makanMustyId}&maxResults=20&key=${youtubeApiKey}`)
+    // .then(response => response.json())
+    // .then(responseJSON => state.pageInfo = responseJSON.pageInfo));
+  }
+  
+  const response = await getYoutubeData();
+  console.log(response, state);
+  
+})
